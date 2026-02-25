cc72ae0cccdec6ce5801780cf154cc3abd27fe2e
catgirlichinose <spigolonluca645@gmail.com>
2026-02-25 10:55:34 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ fi
 
 # INSALLAZIONE SISTEMA BASE
 pacstrap -K /mnt/ base base-devel linux linux-firmware linux-headers grub vim efibootmgr os-prober networkmanager sddm git
+
+# MODIFICA DI SUDOERS PER ABILITARE SUDO
+sudo sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
