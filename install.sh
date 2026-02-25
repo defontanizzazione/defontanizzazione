5d181793d8c2c4d0cb95d0f84a6672fdb9d0c9ca
Wither <120669134+catgirlichinose@users.noreply.github.com>
2026-02-25 12:45:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 # INSALLAZIONE SISTEMA BASE
 pacman -Sy archlinux-keyring --needed
-pacstrap -K /mnt/ base base-devel linux linux-firmware linux-headers grub vim efibootmgr os-prober networkmanager sddm git
+pacstrap -K /mnt/ base base-devel linux linux-firmware linux-headers grub vim efibootmgr os-prober networkmanager sddm git zsh
 
 # MODIFICA DI SUDOERS PER ABILITARE SUDO
 sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /mnt/etc/sudoers > /mnt/etc/sudoers.tmp
@@ -28,7 +28,7 @@ sed 's/#it_IT.UTF-8/it_IT.UTF-8/g' /mnt/etc/locale.gen > /mnt/etc/locale.gen.tmp
 cat /mnt/etc/locale.gen.tmp > /mnt/etc/locale.gen
 rm /mnt/etc/locale.gen.tmp
 
-arch-chroot /mnt locale-gen && localectl set-locale it_IT.UTF-8 && localectl set-keymap it
+arch-chroot /mnt bash -c 'locale-gen && localectl set-locale it_IT.UTF-8 && localectl set-keymap it'
 
 ln -sf ../mnt/usr/share/zoneinfo/Europe/Rome /mnt/etc/localtime
 
@@ -44,4 +44,4 @@ cp -r openSUSE /mnt/usr/share/grub/themes
 rm /mnt/etc/default/grub
 cp grub /mnt/etc/default/
 
-arch-chroot /mnt grub-install --efi-directory=/boot/ && grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt bash -c 'grub-install --efi-directory=/boot/ && grub-mkconfig -o /boot/grub/grub.cfg'
