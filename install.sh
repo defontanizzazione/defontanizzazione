3c79f89723716575ddff7d4f43e239cc94d84854
catgirlichinose <spigolonluca645@gmail.com>
2026-02-25 11:06:30 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ fi
 pacstrap -K /mnt/ base base-devel linux linux-firmware linux-headers grub vim efibootmgr os-prober networkmanager sddm git
 
 # MODIFICA DI SUDOERS PER ABILITARE SUDO
-sudo sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+sudo sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /mnt/etc/sudoers
