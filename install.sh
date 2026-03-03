92346ecf62062c5b8a10c3f1a539a4364c0c21d7
pietrofontanaUwU <matteofrociodimerda@gmail.com>
2026-03-03 09:07:53 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,5 +79,7 @@ arch-chroot /mnt bash -c 'locale-gen'
 arch-chroot /mnt bash -c 'systemd-firstboot --locale=it_IT.UTF-8 --locale-messages=it_IT.UTF-8 --keymap=it'
 ln -sf ../mnt/usr/share/zoneinfo/Europe/Rome /mnt/etc/localtime
 
+echo "finito, fai 'reboot' per continuare"
+
 # Magari eviterei di fare il reboot automatico con lo stato in qui è questo script rn
 # reboot
