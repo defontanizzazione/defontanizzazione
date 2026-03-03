1edfaa15b5fc3bdc4c167a62130d92b7fceeb392
Wither <120669134+catgirlichinose@users.noreply.github.com>
2026-03-03 11:34:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,8 @@ copy_configs () {
     # sfondi
     rm -rf /mnt/usr/share/wallpapers/Next
     cp -r wallpapers/* /mnt/usr/share/wallpapers
-    sed 's/Next/openSUSEdefault/g' /mnt/usr/share/look-and-feel/org.kde.breeze.desktop/metadata.json > /mnt/usr/share/look-and-feel/org.kde.breeze.desktop/metadata.json.tmp
+    touch /mnt/usr/share/look-and-feel/org.kde.breeze.desktop/metadata.json.tmp
+sed 's/Next/openSUSEdefault/g' /mnt/usr/share/look-and-feel/org.kde.breeze.desktop/metadata.json > /mnt/usr/share/look-and-feel/org.kde.breeze.desktop/metadata.json.tmp
     cat /mnt/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/metadata.json.tmp > /mnt/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/metadata.json
     rm /mnt/usr/share/plasma/look-and-feel/org.kde.breeze.desktop/metadata.json.tmp
 
